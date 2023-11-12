33400fde92474aa4126c12c73cab3613d28f9493
its-kayes <kayes.ek8@gmail.com>
2023-11-13 00:42:47 +0600

    Make Announcement with martials

--- src/modules/announcement/announcement.service.ts
+++ src/modules/announcement/announcement.service.ts
@@ -13,7 +13,7 @@ const CreateAnnouncement = async (announcement: IAnnouncement) => {
   if (!isCodeOk)
     throw new AppError('Class code is not valid', httpStatus.UNAUTHORIZED);
 
-  // TODO: Also Validate isRightUser ?
+  // TODO: Also Validate if user have permission to make announcement ?
   // TODO: Date issue fix(BD time zone implement) ?
 
   const makeAnnouncement = await Announcement.create(announcement);
@@ -53,7 +53,7 @@ const CreateAnnouncementWithMaterials = async (data: {
   if (data.materials) {
     const audioFileLinks = await Promise.all(
       data.materials.map(async file => {
-        const link = await bulkUpload(file.name, file.buffer, file.mimetype);
+        const link = await bulkUpload(file.name, file.buffer, file.mimetype); //TODO: File size reduce (sharp)
         return {
           url: link as string,
         };
@@ -63,7 +63,15 @@ const CreateAnnouncementWithMaterials = async (data: {
     announcement.materials = audioFileLinks;
   }
 
-  return announcement;
+  const save = await Announcement.create(announcement);
+  if (!save || !save._id)
+    throw new AppError('Announcement not created', httpStatus.BAD_REQUEST);
+
+  return {
+    description: save.description,
+    materials: save.materials,
+    date: save.date,
+  };
 };
 
 export const AnnouncementService = {
